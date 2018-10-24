4074e212db63499635b1f8092b8ea89d6ee0a594
niravtrivedi21 <niravtrivedi2112@gmail.com>
2018-10-24 11:10:27 +0530

    Node Mongoose part 2

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly  to server");
 
-    var newDish = Dishes({
+    Dishes.create({
         name:'Uthappizza',
         description:'test'
-    });
-
-    newDish.save().then((dish) => {
+    })
+    .then((dish) => {
         console.log(dish);
 
        return Dishes.find({}).exec();
